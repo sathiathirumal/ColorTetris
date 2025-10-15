56e934805b5f16699ac2a1a7d4e266e28a25b4fb
Sat Thiru <satthiru@gmail.com>
2025-10-15 15:52:02 -0400

    Initial Commit

---
+++ ColorTetris/ColorTetrisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorTetrisApp.swift
+//  ColorTetris
+//
+//  Created by Sathia Thirumal on 10/15/25.
+//
+
+import SwiftUI
+
+@main
+struct ColorTetrisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
